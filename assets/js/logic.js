38a01bf5e43a9b46575372aa511926bb551c1307
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-04 13:51:49 +0000

    Add variable for questions and answers

---
+++ assets/js/logic.js
@@ -0,0 +1,3 @@
+// If statement for questions
+// if questions[array number].answers === questions[array number].correct === 2; proceed to next question
+// else --10 from timer variables
---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+// Create variable called questions, with an array to hold and for each question. Each questions stores possible answers in an array.
+ 
+var questions = [{
+question: "Who is JSON?",
+answers: ["he's a boke", "he doesn't exist", "some Javascript conecept"],
+correct: 2,
+},
+
+{
+question: "Who is Robert?",
+answers: ["he's a boke", "he doesn't exist", "no idea"],
+correct: 2,
+}]
