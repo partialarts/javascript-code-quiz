48d2052b385248c53f2a16d07598885b119c223d
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-06 19:19:56 +0000

    add final questions and finish quiz final score screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -19,72 +19,62 @@ var timer = 0;
 
 // The startQuiz function is called when startButton is clicked
 function startQuiz() {
-  startScreenEl.style.display = "none";
-  questionsEl.setAttribute("class", "start");
-  getQuestion(questionIndex);
-  startCountdown();
+    startScreenEl.style.display = "none"; // hides the start screen DOM node
+    questionsEl.setAttribute("class", "start"); // Shows the questions DOM node
+    getQuestion(questionIndex); // Runs the function to get the first question
+    startCountdown(); // Starts the countdown timer function
+}
+
+// The endQuiz function is called when the timer runs out or all the questions are answered
+function endQuiz() {
+    clearInterval(timer); // clears the timer
+    timerEl.textContent = 0; // sets timer text to 0
+    questionsEl.style.display = "none"; // hides the quesions DOM node
+    endScreenEl.setAttribute("class", "start"); // shows the end-screen DOM node
+    if (timeLeft < 0) { // Conditional to change the score to 0 if it is a negative integer
+        finalScoreEl.textContent = 0;
+    } else { // Otherwise display the score using the timeLeft variable.
+        finalScoreEl.textContent = timeLeft;
+    }
 }
 
 // Timer that counts down from 60
 function startCountdown() {
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-      timer = setInterval(function() {
-      // Set the `textContent` of `timerEL` to show the remaining seconds and decrement 'timeLeft' by 1
-      timeLeft--;
-      timerEl.textContent = timeLeft;
-      // Conditional statement to show the remaining time left as long as countdownTimer is greater than 1
-      if(timeLeft === 0) {
-        endQuiz();
-      }
+    timer = setInterval(function () { // The `setInterval()` method calls a function to be executed every 1000 milliseconds
+        timeLeft--; // decrement 'timeLeft' by 1
+        timerEl.textContent = timeLeft; // Set the `textContent` of `timerEL` to show the remaining seconds
+        // Conditional statement to run the endQuiz() function if the score drops to 0
+        if (timeLeft <= 0) {
+            endQuiz();
+        }
     }, 1000);
-  }
+}
 
-  function getQuestion(currentQuestion) {
+
+function getQuestion(currentQuestion) {
     if (currentQuestion < questions.length) {
         questionsTitle.textContent = questions[currentQuestion].question;
         for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
-          var button = document.createElement("button");
-          button.setAttribute("data-state", i);
-          button.textContent = questions[currentQuestion].answers[i];
-          questionsChoices.appendChild(button);
+            var button = document.createElement("button");
+            button.setAttribute("data-state", i);
+            button.textContent = questions[currentQuestion].answers[i];
+            questionsChoices.appendChild(button);
         }
-    } else 
+    } else {
         endQuiz();
-  }   
-
-function endQuiz() {
-    clearInterval(timer);
-    timerEl.textContent = 0;
-    questionsEl.style.display = "none";
-    endScreenEl.setAttribute("class", "start");
-    finalScoreEl.textContent = timeLeft;
-  }
-
-// Timer that counts down from 60
-function startCountdown() {
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    timer = setInterval(function() {
-    // Set the `textContent` of `timerEL` to show the remaining seconds and decrement 'timeLeft' by 1
-    timeLeft--;
-    timerEl.textContent = timeLeft;
-    // Conditional statement to show the remaining time left as long as countdownTimer is greater than 1
-    if(timeLeft === 0) {
-      endQuiz();
     }
-  }, 1000);
 }
 
-function clearFeedback() { 
-    feedbackEl.setAttribute ("class", "feedback hide"); // clears question feedback from the screen 
+function clearFeedback() {
+    feedbackEl.setAttribute("class", "feedback hide"); // clears question feedback from the screen 
 }
 
 // Attach event listener to questionChoices buttons and validate whether answer is correct
-questionsChoices.addEventListener("click", function(event) {
+questionsChoices.addEventListener("click", function (event) {
     feedbackEl.setAttribute("class", "feedback");
     var chosenButton = event.target;
     if (chosenButton.matches("button")) {
         var answerData = chosenButton.getAttribute("data-state");
-        console.log(answerData)
         if (answerData == questions[questionIndex].correct) {
             feedbackEl.textContent = "Correct!"
         } else {
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,24 +1,29 @@
 // Create variable called questions, with an array to hold and for each question. Each questions stores possible answers in an array.
- 
+
 var questions = [
-{
-question: "What is JSON?",
-answers: ["1. AI that writes code for you", "2. His name is spelled Jason!", "3. A format for storing and transporting data"],
-correct: 2
-},
-{
-question: "What is the DOM?",
-answers: ["1. Document Object Model", "2. Dominic, Dom for short", "3. Document Organisation Method"],
-correct: 0
-},
-{
-question: "Who is Robert?",
-answers: ["1. He's a boke", "2. He doesn't exist", "3. No idea"],
-correct: 2
-},
-{
-question: "Who is Robert?",
-answers: ["1. He's a boke", "2. He doesn't exist", "3. No idea"],
-correct: 2
-},
+    {
+        question: "What is JSON?",
+        answers: ["1. AI that writes code for you", "2. His name is spelled Jason!", "3. A format for storing and transporting data", "4. JavaScript Set Object Node"],
+        correct: 2
+    },
+    {
+        question: "What is the DOM?",
+        answers: ["1. Document Object Model", "2. Dominic, Dom for short", "3. Document Organisation Method", "4. Diverse Other Methodologies"],
+        correct: 0
+    },
+    {
+        question: "What is a method?",
+        answers: ["1. Another word for syntax", "2. A method is a function associated with an object property", "3. A block of code designed to create an array from a key", "4. All of the above"],
+        correct: 1
+    },
+    {
+        question: "What is a conditional statement?",
+        answers: ["1. Used to perform different actions based on different conditions.", "2. It only works in certain weather conditions", "3. Can change its code depending on the time of day", "4. All of the above"],
+        correct: 0
+    },
+    {
+        question: "Which of these it true about a function()?",
+        answers: ["1. Defined with the function keyword", "2. Parameters are listed inside the parentheses", "3. Arguments are the values received by the function when it is invoked", "4. All of the above"],
+        correct: 3
+    }
 ];
